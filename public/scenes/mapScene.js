3a528091861f85e4006af2e2ff368231a9d137d7
D3icidal <D3icidal@gmail.com>
2020-03-22 16:08:37 -0600

    adds working example in mapScene.js of multiple layers, multiple tilesets

--- public/scenes/mapScene.js
+++ public/scenes/mapScene.js
@@ -27,9 +27,23 @@ export default class MapScene extends Phaser.Scene {
 
       // this.load.tilemapCSV('platformMap', '../assets/tilesets/platform/platformMap_MainPlatform.csv');
 
+
+
+// WORKING!!!!
     this.load.image('tiles', '../assets/tilesets/platform/tileset.png');
+    this.load.image('island', '../assets/tilesets/platform/far-grounds.png');
 
     this.load.tilemapTiledJSON('test', '../assets/tilesets/platform/simplifiedTest.json');
+
+
+
+// end of working example for loading
+
+  // this.load.tilemapTiledJSON('test', '../assets/tilesets/platform/resumePlatform_Tiled_CSVENCODING.json');
+// resumePlatform_Tiled_CSVENCODING
+
+
+
 
     //does images exist
     // console.log(this.textures.exists('tileImageSet'));
@@ -41,9 +55,17 @@ export default class MapScene extends Phaser.Scene {
 
     const map = this.make.tilemap({ key: "test" });
 
-    const tileset = map.addTilesetImage("tileset", "tiles");
+    //ISLAND
+    const island = map.addTilesetImage("island", "island");
+    map.createStaticLayer('island', island, 0, 200);
 
+    const tileset = map.addTilesetImage("tileset", "tiles");
     const platforms = map.createStaticLayer('MainLayer', tileset, 0, 200);
+
+    map.createStaticLayer('trees', tileset, 0, 200);
+
+
+
     // debugger;
 
   	// var tileset = map.addTilesetImage('tileset','tiles');
