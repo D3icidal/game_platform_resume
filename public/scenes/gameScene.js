bc279be797fe05ce79c7a7ba0bb2b15649ff04e8
D3icidal <D3icidal@gmail.com>
2020-03-23 15:39:03 -0600

    comments out a lot of currently unused code to clean up file

--- public/scenes/gameScene.js
+++ public/scenes/gameScene.js
@@ -101,8 +101,8 @@ export default class GameScene extends Phaser.Scene {
 
 
     // The player and its settings
-    this.player = this.physics.add.sprite(75,150,'player')
-    player.setBounce(0.2); // our player will bounce from items
+    const player = this.physics.add.sprite(75,150,'player');
+    player.setBounce(0.1); // our player will bounce from items
     player.setCollideWorldBounds(true); // don't go out of the map
     // this.player = this.physics.add.sprite(200, 100, 'player');
 
@@ -110,7 +110,8 @@ export default class GameScene extends Phaser.Scene {
     // this.player.setBounce(0.2);
     // this.player.setCollideWorldBounds(true);
 
-
+    //  Input Events
+    this.cursors = this.input.keyboard.createCursorKeys();
 
     // // set background color, so the sky is not black
     // this.cameras.main.setBackgroundColor('#ccccff');
@@ -120,147 +121,96 @@ export default class GameScene extends Phaser.Scene {
 
 
     //  Our player animations, turning, walking left and walking right.
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', {
-        start: 0,
-        end: 3
-      }),
-      frameRate: 10,
-      repeat: -1
-    });
+    // this.anims.create({
+    //   key: 'left',
+    //   frames: this.anims.generateFrameNumbers('dude', {
+    //     start: 0,
+    //     end: 3
+    //   }),
+    //   frameRate: 10,
+    //   repeat: -1
+    // });
+    //
+    // this.anims.create({
+    //   key: 'turn',
+    //   frames: [{
+    //     key: 'dude',
+    //     frame: 4
+    //   }],
+    //   frameRate: 20
+    // });
+    //
+    // this.anims.create({
+    //   key: 'right',
+    //   frames: this.anims.generateFrameNumbers('dude', {
+    //     start: 5,
+    //     end: 8
+    //   }),
+    //   frameRate: 10,
+    //   repeat: -1
+    // });
 
-    this.anims.create({
-      key: 'turn',
-      frames: [{
-        key: 'dude',
-        frame: 4
-      }],
-      frameRate: 20
-    });
 
-    this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('dude', {
-        start: 5,
-        end: 8
-      }),
-      frameRate: 10,
-      repeat: -1
-    });
-
-    //  Input Events
-    this.cursors = this.input.keyboard.createCursorKeys();
 
     //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-    this.stars = this.physics.add.group({
-      key: 'star',
-      // repeat: 11,
-      repeat: 3,
-      setXY: {
-        x: 12,
-        y: 0,
-        stepX: 70
-      }
-    });
+    // this.stars = this.physics.add.group({
+    //   key: 'star',
+    //   // repeat: 11,
+    //   repeat: 3,
+    //   setXY: {
+    //     x: 12,
+    //     y: 0,
+    //     stepX: 70
+    //   }
+    // });
 
-    this.stars.children.iterate(function(child) {
-
+    // this.stars.children.iterate(function(child) {
       //  Give each star a slightly different bounce
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+      // child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
 
-    });
-
-    this.bombs = this.physics.add.group();
+    // });
 
     //  The score
-    this.scoreText = this.add.text(16, 16, 'score: 0', {
-      fontSize: '32px',
-      fill: '#000'
-    });
+    // this.scoreText = this.add.text(16, 16, 'score: 0', {
+    //   fontSize: '32px',
+    //   fill: '#000'
+    // });
 
     //  Collide the player and the stars with the platforms
-    this.physics.add.collider(this.player, this.platforms);
-    this.physics.add.collider(this.stars, this.platforms);
-    this.physics.add.collider(this.bombs, this.platforms);
+    // this.physics.add.collider(this.player, this.platforms);
+    // this.physics.add.collider(this.stars, this.platforms);
+    // this.physics.add.collider(this.bombs, this.platforms);
 
     //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
-
-    this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
+    // this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
+    //
+    // this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
   }
 
   update() {
-    console.log("gameScene update");
+    // console.log("gameScene update");
     // if (gameOver) {
     //   return;
     // }
 
-    if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-160);
-
-      this.player.anims.play('left', true);
-    } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(160);
-
-      this.player.anims.play('right', true);
-    } else {
-      this.player.setVelocityX(0);
-
-      this.player.anims.play('turn');
-    }
-
-    if (this.cursors.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330);
-    }
+    // if (this.cursors.left.isDown) {
+    //   this.player.setVelocityX(-160);
+    //
+    //   this.player.anims.play('left', true);
+    // } else if (this.cursors.right.isDown) {
+    //   this.player.setVelocityX(160);
+    //
+    //   this.player.anims.play('right', true);
+    // } else {
+    //   this.player.setVelocityX(0);
+    //
+    //   this.player.anims.play('turn');
+    // }
+    //
+    // if (this.cursors.up.isDown && this.player.body.touching.down) {
+    //   this.player.setVelocityY(-330);
+    // }
   }
-
-
-
-  collectStar(player, star) {
-    star.disableBody(true, true);
-
-    //  Add and update the score
-    this.score += 10;
-    this.scoreText.setText('Score: ' + this.score);
-
-    if (this.stars.countActive(true) === 0) {
-      //  A new batch of stars to collect
-      this.stars.children.iterate(function(child) {
-
-        child.enableBody(true, child.x, 0, true, true);
-
-      });
-
-      var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-      this.bomb = this.bombs.create(x, 16, 'bomb');
-      this.bomb.setBounce(1);
-      this.bomb.setCollideWorldBounds(true);
-      this.bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-      this.bomb.allowGravity = false;
-
-    }
-  }
-
-  hitBomb(player, bomb) {
-    this.physics.pause();
-
-    player.setTint(0xff0000);
-
-    player.anims.play('turn');
-
-    this.gameOver = true;
-  }
-
-
-  end() {
-
-  }
-
-
-
 
 }
-
 // export default GameScene;
