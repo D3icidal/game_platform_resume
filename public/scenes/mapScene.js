5c943318d402934ab593eab54ef2fa72b067bb70
D3icidal <D3icidal@gmail.com>
2020-03-23 11:27:45 -0600

    adds more layers and tilesets for platform and decor

--- public/scenes/mapScene.js
+++ public/scenes/mapScene.js
@@ -39,8 +39,10 @@ export default class MapScene extends Phaser.Scene {
 
 
 this.load.image('tiles', '../assets/tilesets/platform/tileset.png');
-this.load.image('sea', '../assets/tilesets/platform/sea.png');
+this.load.image('sky', '../assets/tilesets/platform/sky.png');
 this.load.image('clouds', '../assets/tilesets/platform/clouds.png');
+this.load.image('sea', '../assets/tilesets/platform/sea.png');
+this.load.image('island', '../assets/tilesets/platform/far-grounds.png');
 
 this.load.tilemapTiledJSON('tiledTilemap', '../assets/tilesets/platform/resumePlatformerV2.json');
   }
@@ -50,6 +52,11 @@ this.load.tilemapTiledJSON('tiledTilemap', '../assets/tilesets/platform/resumePl
     console.log("mapScene create");
 
     const map = this.make.tilemap({ key: "tiledTilemap" });
+
+
+    //sky layer
+    const skyImage = map.addTilesetImage("sky_tileset", "sky");
+    map.createStaticLayer('Sky_Layer', skyImage, 0, 0);
 
 
 
@@ -63,15 +70,27 @@ this.load.tilemapTiledJSON('tiledTilemap', '../assets/tilesets/platform/resumePl
     const seaTilesetImage = map.addTilesetImage("sea_tileset", "sea");
     const sea_layer = map.createStaticLayer('Sea_Layer', seaTilesetImage, 0, 0);
 
+    //island layer
+    const islandImage = map.addTilesetImage("island_tileset", "island");
+    map.createStaticLayer('Island_Layer', islandImage, 0, 0);
+
+    //Mist forground clouds layer
+    map.createStaticLayer('MistCloud_Layer', cloudsImage, 0, 0);
+
+
+
+
 
     const tileset = map.addTilesetImage("mysticcliffs_tileset", "tiles");
-    const platforms = map.createStaticLayer('MainPlatform_Layer', tileset, 0, 0);
+
+    //foreground behind actor layer (trees decor)
     map.createStaticLayer('Foreground_BehindActor_Layer', tileset, 0, 0);
 
+    //main platform game layer
+    const platforms = map.createStaticLayer('MainPlatform_Layer', tileset, 0, 0);
 
-
-
-
+    //overlay layer (ladders, bridges, etc) for playform
+    map.createStaticLayer('MainOverlay_Layer', tileset, 0, 0);
 
 
 
